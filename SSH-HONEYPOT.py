714aede70a76a8665b4eacd2bfa544dd025a04f1
TheLeopard65 <Yasirali752550@gmail.com>
2025-01-13 00:49:05 +0500

    Finally Working

--- SSH-HONEYPOT.py
+++ SSH-HONEYPOT.py
@@ -27,32 +27,52 @@ class SSHServer(paramiko.ServerInterface):
     def check_auth_password(self, username: str, password: str) -> int:
         client_ip = self.client_address[0]
         if username in VALID_USERS and VALID_USERS[username] == password:
-	        logging.info(f"[!] SUCCESSFULL AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
-	        print(f"[!] SUCCESSFULL AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
-        	return paramiko.AUTH_SUCCESSFUL
+            self.username = username
+            logging.info(f"[!] SUCCESSFUL AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
+            print(f"[!] SUCCESSFUL AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
+            return paramiko.AUTH_SUCCESSFUL
         else:
-	        logging.info(f"[!] FAILED AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
-	        print(f"[!] FAILED AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
-        	return paramiko.AUTH_FAILED
+            logging.info(f"[!] FAILED AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
+            print(f"[!] FAILED AUTHENTICATION ATTEMPT BY USER: {username}:{password} FROM IP: {client_ip} !!!")
+            return paramiko.AUTH_FAILED
 
     def get_allowed_auths(self, username): return 'password'
-    def check_channel_pty_request( self, channel, term, width, height, pixelwidth, pixelheight, modes ): return True
-    def check_channel_shell_request( self, channel ): return True
+    def check_channel_pty_request(self, channel, term, width, height, pixelwidth, pixelheight, modes): return True
+    def check_channel_shell_request(self, channel): return True
 
     def check_channel_request(self, channel_type, chanid):
-        if channel_type in ['session', 'pty', 'shell']: return paramiko.OPEN_SUCCEEDED
+        if channel_type in ['session', 'pty']: return paramiko.OPEN_SUCCEEDED
         return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def start_shell(self, channel):
         try:
-            channel.get_pty()
             channel.send("[#] WELCOME TO THE SSH HONEYPOT! YOU ARE NOW IN A R-SHELL!\n")
-            channel.invoke_shell()
+            process = subprocess.Popen('/bin/rbash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=False)
+            while True:
+                if not channel.active: break
+                channel.send(f"\r{self.username}@system:~$ ")
+                command = ""
+                while True:
+                    char = channel.recv(1).decode()
+                    if (not char) or (char == '\r') or (char == '\x03') or (char == '\x04'): break
+                    command += char
+                    channel.send(char)
+                command = command.strip()
+                if (command == 'exit') or (command == '^C'): break
+                try:
+                    process = subprocess.Popen(['/bin/rbash', '-c', command], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    stdout, stderr = process.communicate()
+                    if stdout: channel.send(f"\r\n{stdout.decode()}")
+                    if stderr: channel.send(f"\r\n{stderr.decode()}")
+                    if not stdout and not stderr: channel.send(f"\r\nERROR#: COMMAND {command} DID NOT PRODUCE ANY OUTPUT OR WAS INVALID !!!\n")
+                except Exception as error:
+                    channel.send(f"\r\nERROR#: Exception occurred while executing command: {str(error)}\n")
         except Exception as error:
-            logging.error(f"[@] ERROR (COULDN'T START REAL SHELL): {error}")
-            channel.send(f"[@] ERROR (COULDN'T START REAL SHELL): {error}\n")
+            logging.error(f"[@] ERROR (COULDN'T START R-SHELL): {error}")
+            if channel.active: channel.send(f"[@] ERROR (COULDN'T START R-SHELL): {error}\n")
+            print(f"[@] ERROR (COULDN'T START R-SHELL): {error}\n")
         finally:
-            channel.close()
+            if channel.active: channel.close()
 
 def handle_connection(client_sock, client_addr):
     try:
@@ -62,18 +82,21 @@ def handle_connection(client_sock, client_addr):
         transport.add_server_key(server_key)
         ssh = SSHServer(client_sock.getpeername())
         transport.start_server(server=ssh)
-        channel = transport.accept(15)
+        channel = transport.accept(25)
         if channel is None:
-        	logging.error("[@] ERROR (CHHANEL IS NONE) : Connection may have Failed !!!")
-        	print("[@] ERROR (CHHANEL IS NONE) : Connection may have Failed !!!")
-        	return
+            logging.error("[@] ERROR (CHANNEL IS NONE): Connection may have failed !!!")
+            print("[@] ERROR (CHANNEL IS NONE): Connection may have failed !!!")
+            return
         ssh.start_shell(channel)
+    except socket.error as e:
+        logging.error(f"[@] ERROR (SOCKET ERROR): {e}")
+        print(f"[@] ERROR (SOCKET ERROR): {e}")
     except Exception as error:
         logging.error(f"[@] ERROR (EXCEPTION IN HANDLE-CONNECTION FUNCTION): {error}")
         print(f"[@] ERROR (EXCEPTION IN HANDLE-CONNECTION FUNCTION): {error}")
     finally:
-    	if transport and transport.is_active(): transport.close()
-    	client_sock.close()
+        if transport.is_active(): transport.close()
+        client_sock.close()
 
 def start_server(host, port):
     print(f'[#] STARTING SSH HONEYPOT ON PORT : {port} !!!')
@@ -82,28 +105,36 @@ def start_server(host, port):
     server_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_sock.bind((host, port))
     server_sock.listen(16)
-    while True:
-        try:
-            client_sock, client_addr = server_sock.accept()
-            print(f"[#] CONNECTION RECEIVED FROM : ({client_addr[0]}:{client_addr[1]}) !!!")
-            logging.info(f"[#] CONNECTION RECEIVED FROM : ({client_addr[0]}:{client_addr[1]}) !!!")
-            client_thread = threading.Thread(target=handle_connection, args=(client_sock, client_addr))
-            client_thread.start()
-        except KeyboardInterrupt:
-            print("[#] SHUTTING DOWN HONEYPOT -> RECEIVED KEYBOARD-INTERRUPT !!!")
-            logging.info("[#] SHUTTING DOWN HONEYPOT -> RECEIVED KEYBOARD-INTERRUPT !!!")
-            break
-        except Exception as error:
-            print(f"[@] ERROR (COULD ACCEPT CONNECTION) : {error}")
-            logging.error(f"[@] ERROR (COULD ACCEPT CONNECTION) : {error}")
+    try:
+        while True:
+            try:
+                client_sock, client_addr = server_sock.accept()
+                print(f"[#] CONNECTION RECEIVED FROM : ({client_addr[0]}:{client_addr[1]}) !!!")
+                logging.info(f"[#] CONNECTION RECEIVED FROM : ({client_addr[0]}:{client_addr[1]}) !!!")
+                client_thread = threading.Thread(target=handle_connection, args=(client_sock, client_addr))
+                client_thread.start()
+            except socket.error as error:
+                print(f"[@] ERROR (COULD ACCEPT CONNECTION): {error}")
+                logging.error(f"[@] ERROR (COULD ACCEPT CONNECTION): {error}")
+            except Exception as e:
+                print(f"[@] ERROR (UNEXPECTED ERROR): {e}")
+                logging.error(f"[@] ERROR (UNEXPECTED ERROR): {e}")
+    except KeyboardInterrupt:
+        print("[#] SHUTTING DOWN HONEYPOT -> RECEIVED KEYBOARD-INTERRUPT !!!")
+        logging.info("[#] SHUTTING DOWN HONEYPOT -> RECEIVED KEYBOARD-INTERRUPT !!!")
+    finally:
+        server_sock.close()
 
 def main():
     try:
         generate_key()
         start_server(IP_ADDRESS, SSH_PORT)
     except Exception as error:
-        logging.error(f"[@] ERROR (COULDN'T START SERVER) : {error}")
-        print(f"[@] ERROR (COULDN'T START SERVER) : {error}")
+        logging.error(f"[@] ERROR (COULDN'T START SERVER): {error}")
+        print(f"[@] ERROR (COULDN'T START SERVER): {error}")
+    except KeyboardInterrupt:
+        print("[#] SERVER SHUTDOWN GRACEFULLY DUE TO KEYBOARD INTERRUPT.")
+        logging.info("[#] SERVER SHUTDOWN GRACEFULLY DUE TO KEYBOARD INTERRUPT.")
 
 if __name__ == '__main__':
     main()
